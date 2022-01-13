cf29858e8ab9dd9adb27ad5e4df4b74a4f3d244d
ujuujaan <97655747+ujuujaan@users.noreply.github.com>
2022-01-13 11:05:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-beg= True
-pink= False
-buyj= True
+beg= False
+pink=False
+buyj= False
 hunt= True
-fish= True
-pm= True
-bet= True
-slots= True
-hl= True
-dep= True
+fish= False
+pm= False
+bet= False
+slots= False
+hl= False
+dep= False
 #id of the user reciving the pink
 send_id =270904126974590976  #dank memer's id
 
-bet_amount=100
-slots_amount=100
+bet_amount=0
+slots_amount=0
 hl_answer= "l"  # i felt like low is more common
